1ff94b66e580a7566e1b7eae88be1bc12a3a794f
unknown <amperezpe@gmail.com>
2022-08-22 17:08:32 -0500

    creacion de archivos de electronjs

---
+++ electron-app/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron'); //importar libreria de electron
+const path = require('path'); // importar libreria path
+
+// funcion para crear una nueva ventana
+const createWindow = () => {
+    const win = new BrowserWindow({
+        height: 500, 
+        width: 800, 
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile("index.html")
+};
+
+
+//crear la ventana cuando la aplicacion este lista
+app.whenReady().then(()=> {
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+      });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  });
